3ebb8fe4c31962e7ca03705013605006c1df4487
Cuttlefish <zh4n@live.co.uk>
2017-11-26 00:29:13 +0000

    Early version
    - updated all pages
    - nav buttons work with js
    - added a bunch of content

--- js/main.js
+++ js/main.js
@@ -1,75 +1,125 @@
 $(function(){
 
-  $(".footer .rights").load("footer.html");
+  // beutifull
 
-    //mobile menu toggling
-    $("#menu_icon").click(function(){
-        $("header nav ul").toggleClass("show_menu");
-        $("#menu_icon").toggleClass("close_menu");
-        return false;
-    });
+  addNavLinks();
 
+  addFooter();
 
+  bindBtns();
 
-    //Contact Page Map Centering
+  centerContactPage();
+
+  //addTooltips();
+
+  function addFooter() {
+    $(".footer .rights").load("footer.html");
+  }
+
+  function bindBtns() {
+
+    $("#menu_icon").click(toggleMenu);
+
+  }
+
+  function toggleMenu(e) {
+    e.preventDefault(); // this is so the # doesn't show up in the address bar
+    $("header nav ul").toggleClass("show_menu");
+    $("#menu_icon").toggleClass("close_menu");
+  }
+
+  function centerContactPage() {
     var hw = $('header').width() + 50;
     var mw = $('#map').width();
     var wh = $(window).height();
     var ww = $(window).width();
 
     $('#map').css({
-        "max-width" : mw,
-        "height" : wh
+      "max-width" : mw,
+      "height" : wh
     });
 
     if(ww>1100){
-         $('#map').css({
-            "margin-left" : hw
-        });
+      $('#map').css({
+        "margin-left" : hw
+      });
+    }
+  }
+
+  function addTooltips() {
+    $("a").mouseover(onMouseOverTooltip);
+    $("a").mouseover(onMouseOutTooltip);
+
+  }
+
+  function onMouseOverTooltip() {
+    var attr_title = $(this).attr("data-title");
+
+    if( attr_title == undefined || attr_title == "") return false;
+
+    $(this).after('<span class="tooltip"></span>');
+
+    var tooltip = $(".tooltip");
+    tooltip.append($(this).data('title'));
+
+
+    var tipwidth = tooltip.outerWidth();
+    var a_width = $(this).width();
+    var a_hegiht = $(this).height() + 3 + 4;
+
+    //if the tooltip width is smaller than the a/link/parent width
+    if(tipwidth < a_width){
+      tipwidth = a_width;
+      $('.tooltip').outerWidth(tipwidth);
     }
 
+    var tipwidth = '-' + (tipwidth - a_width)/2;
+    $('.tooltip').css({
+      'left' : tipwidth + 'px',
+      'bottom' : a_hegiht + 'px'
+    }).stop().animate({
+      opacity : 1
+    }, 200);
+  }
 
+  function onMouseOutTooltip() {
+    var tooltip = $(".tooltip");
+    tooltip.remove();
+  }
 
+  function addNavLinks() {
 
+    // If the nav btns are not found, skip this function
+    if ( $('.work_nav a.previous').length === 0)  return;
 
-    //Tooltip
-    $("a").mouseover(function(){
+    var myPages = [
+      '/showreel.html',
+      '/opengl.html',
+      '/cosmic_cadence.html',
+      '/vine_venture.html',
+      '/mindfork.html',
+      '/jelly_journey.html',
+      '/flocking_ai.html',
+      '/hexfall.html',
+      '/all_systems_go.html',
+      '/about_me.html',
+      '/dissertation.html'
+    ];
 
-        var attr_title = $(this).attr("data-title");
+    var currPage = location.pathname;
 
-        if( attr_title == undefined || attr_title == "") return false;
+    var currIndex = myPages.indexOf(currPage);
 
-        $(this).after('<span class="tooltip"></span>');
+    var prev = currIndex - 1;
+    if (prev === -1 ) prev = myPages.length-1;
+    var next = currIndex + 1;
+    if (next === myPages.length ) next = 0;
 
-        var tooltip = $(".tooltip");
-        tooltip.append($(this).data('title'));
+    // You can later add the modulos thing so it loops itself
+    $('.work_nav a.previous').attr('href', myPages[prev] );
+    $('.work_nav a.next').attr('href', myPages[next] );
 
-
-        var tipwidth = tooltip.outerWidth();
-        var a_width = $(this).width();
-        var a_hegiht = $(this).height() + 3 + 4;
-
-        //if the tooltip width is smaller than the a/link/parent width
-        if(tipwidth < a_width){
-            tipwidth = a_width;
-            $('.tooltip').outerWidth(tipwidth);
-        }
-
-        var tipwidth = '-' + (tipwidth - a_width)/2;
-        $('.tooltip').css({
-            'left' : tipwidth + 'px',
-            'bottom' : a_hegiht + 'px'
-        }).stop().animate({
-            opacity : 1
-        }, 200);
-
-
-    });
-
-    $("a").mouseout(function(){
-        var tooltip = $(".tooltip");
-        tooltip.remove();
-    });
+  }
 
 
 });
